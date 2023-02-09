3faa8a79f6739b040c4b92a041b372bd5f0932c5
Victoria Sales <contatovictoriasales@gmail.com>
2023-02-09 18:54:26 -0300

    Fazendo rota para carrinho (teste)

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Search from '../pages/Search';
+import ShoppingCart from '../pages/ShoppingCart';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
+        <Route
+          exact
+          path="/shoppingcart"
+          render={ () => (
+            <ShoppingCart />
+          ) }
+        />
         <Route
           exact
           path="/"
