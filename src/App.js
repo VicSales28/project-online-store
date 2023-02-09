9d68197aaabc3c110858a4bdb0f06a29ce1e85c2
Victoria Sales <contatovictoriasales@gmail.com>
2023-02-09 19:41:41 -0300

    Altera arquivo de rotas e cria carrinho (Req03)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Search from './pages/Search';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Search from './pages/Search';
+import ShoppingCart from './pages/ShoppingCart';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div>
-      <Search />
+      <Switch>
+        <Route exact path="/shoppingcart" component={ ShoppingCart } />
+        <Route exact path="/" component={ Search } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
     </div>
   );
 }
