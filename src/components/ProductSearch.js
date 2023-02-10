d1c1d79c4210784968fad58f316c449206f27e3d
brunagimenez <brunagimenez97@gmail.com>
2023-02-10 12:25:45 -0300

    iniciando req5

---
+++ src/components/ProductSearch.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProductSearch extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+            <input />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ProductSearch;
