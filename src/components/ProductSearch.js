91925ce796d65cc7f7fd15a7b3da9fac8fbff9c9
brunagimenez <brunagimenez97@gmail.com>
2023-02-10 15:32:00 -0300

    capturei input de pesquisa de produto

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 
 class ProductSearch extends Component {
+  state = {
+    valueInput: '',
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { valueInput } = this.state;
     return (
       <div>
         <form>
-            <input
-              type="text"
-              data-testid="query-input"
-            />
-            <button
-              type="button"
-              data-testid="query-button"
-            >
-              Buscar
-            </button>
+          <input
+            type="text"
+            data-testid="query-input"
+            value={ valueInput }
+            onChange={ this.onInputChange }
+            name="valueInput"
+          />
+          <button
+            type="button"
+            data-testid="query-button"
+          >
+            Buscar
+          </button>
         </form>
       </div>
     );
