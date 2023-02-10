e85de12f4e625900c8c754c5b6e7ca4448fc9782
Victoria Sales <contatovictoriasales@gmail.com>
2023-02-10 06:42:37 -0300

    Lista categorias de produtos via API

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,51 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 import CartBtn from '../components/CartBtn';
 
 class Search extends React.Component {
+  state = {
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const data = await getCategories();
+    // console.log(data);
+
+    this.setState({
+      categories: data,
+    });
+  };
+
   render() {
+    const { categories } = this.state;
+
     return (
       <div>
+
         <section data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </section>
+
+        <section>
+          {
+            categories.map((category) => (
+              <button
+                key={ category.id }
+                data-testid="category"
+                type="button"
+              >
+                { category.name }
+              </button>
+            ))
+          }
+        </section>
+
         <CartBtn />
+
       </div>
     );
   }
